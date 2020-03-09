bdda14f8e8ead7bc4ad37a41f025f64e897853c7
Jonathan Rawet <Jonathan@rawet.se>
2020-03-09 15:00:45 +0100

    adding a player prefab with somewhat working movement

---
+++ Bachelor_Degree_2020_Artefact/Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform = null;
+    [SerializeField] private float sensitivity = 1.0f;
+    [SerializeField, Range(0.0f, 89.0f)] private float maxXDegrees = 89.0f;
+
+    private Vector3 firstPersonOffset = new Vector3(0.0f, 0.5f, 0.0f);
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        //Quaternion newRotation = UpdateRotation();
+
+        transform.rotation = UpdateRotation();
+        transform.position = playerTransform.position + firstPersonOffset;
+
+    }
+
+
+    private Quaternion UpdateRotation()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensitivity;
+
+        rotationY += mouseX;
+        rotationX -= mouseY;
+
+        rotationX = Mathf.Clamp(rotationX, -maxXDegrees, maxXDegrees);
+
+        return Quaternion.Euler(rotationX, rotationY, 0.0f);
+
+    }
+}
--- Bachelor_Degree_2020_Artefact/Assets/Scripts/PlayerMovement.cs
+++ Bachelor_Degree_2020_Artefact/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float movementSpeed = 1.0f;
+    [SerializeField] private float gravity = -9.81f;
+
+    
+
+    [SerializeField] private Vector3 inputVector = Vector3.zero;
+    private CharacterController characterController;
+    private Transform mainCameraTransform;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        mainCameraTransform = Camera.main.transform;
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (characterController.isGrounded)
+        {
+            inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+
+            //Vector3 topPoint = transform.position + characterController.center + Vector3.up * (characterController.height / 2 - characterController.radius);
+            //Vector3 bottomPoint = transform.position + characterController.center - Vector3.up * (characterController.height / 2 - characterController.radius);
+
+            //Physics.CapsuleCast(topPoint, bottomPoint, characterController.radius, Vector3.down, out RaycastHit raycastHit, Mathf.Infinity, ~0, QueryTriggerInteraction.Ignore);
+
+            //Debug.DrawLine(Vector3.zero, raycastHit.normal, Color.red, 1.0f);
+            ////Debug.Log(raycastHit.normal);
+
+            //inputVector = Vector3.ProjectOnPlane(mainCameraTransform.rotation * inputVector, raycastHit.normal).normalized * inputVector.magnitude;
+            inputVector = Vector3.ProjectOnPlane(mainCameraTransform.rotation * inputVector, Vector3.up).normalized * inputVector.magnitude;
+
+            inputVector = Vector3.ClampMagnitude(inputVector, 1.0f) * movementSpeed;
+
+            
+
+        }
+        inputVector += new Vector3(0.0f, gravity, 0.0f) * Time.deltaTime;
+
+        characterController.Move(inputVector * Time.deltaTime);
+
+        Debug.DrawLine(transform.position, transform.position + (inputVector * 2.0f), Color.red);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("wow.");
     }
 }
